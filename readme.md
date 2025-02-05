02a3fcb3e8ba5655922677d2155b346f585e8584
Patrick Reis Andrade <andrade.patrickreis@gmail.com>
2025-02-05 12:40:24 -0300

    AULA W08 - EVENTOS DE GRAVAÇÃO DO MICROFONE E TIMER DE GRAVAÇÃO- Configurei o evento para iniciar o audio do microfone, configurei os botões para cancelar o audio e enviar o audio, também incluir o timer do audio, mas ainda não esta formatado na maneira correta, para contar o tempo do audio criei um metodo que realiza a contagem para mim e cada vez que clico para enviar um audio chamo este metodo.

--- readme.md
+++ readme.md
@@ -1,14 +1,14 @@
 # **Projeto WhatsApp Clone** 📱📞
 
-Este é o acompanhamento do progresso no curso de **Projeto WhatsApp Clone**. O curso tem um total de **50 aulas**, e atualmente estou na **aula 7**.
+Este é o acompanhamento do progresso no curso de **Projeto WhatsApp Clone**. O curso tem um total de **50 aulas**, e atualmente estou na **aula 8**.
 
 ---
 
 ## **Status do Curso**
 
 ### Progresso Atual  
-#### [███░░░░░░░░░░░░░░░░░░░░] **14%**  
-- Aulas concluídas: **7 de 50**  
+#### [███░░░░░░░░░░░░░░░░░░░░] **16%**  
+- Aulas concluídas: **8 de 50**  
 
 ### Bora codar 🚀💻!
 
--- src/controller/WhatsAppController.js
+++ src/controller/WhatsAppController.js
@@ -174,81 +174,115 @@ class WhatsAppController { // Criando a classe controller do WhatsApp
             document.addEventListener('click', this.closeMenuAttach.bind(this)) // Adiciona evento para fechar o menu ao clicar fora
         })
 
-         // Eventos para os botões de anexos (apenas exibem mensagens no console por enquanto)
-         this.el.btnAttachPhoto.on('click', e =>{
+        // Eventos para os botões de anexos (apenas exibem mensagens no console por enquanto)
+        this.el.btnAttachPhoto.on('click', e => {
             this.el.inputPhoto.click(); // simula o click no botão
-         });
-         
-         // aqui ele percorre toda a coleção com o spread, buscando cada file
-         this.el.inputPhoto.on('change', e=>{
+        });
+
+        // aqui ele percorre toda a coleção com o spread, buscando cada file
+        this.el.inputPhoto.on('change', e => {
             console.log(this.el.inputPhoto.files);
 
-            [...this.el.inputPhoto.files].forEach(file=>{
+            [...this.el.inputPhoto.files].forEach(file => {
                 console.log(file)
             })
-         })
+        })
 
-          // abre o botão da camera e remove outros elementos que estao no closeAllMainPanel
-         this.el.btnAttachCamera.on('click', e =>{
+        // abre o botão da camera e remove outros elementos que estao no closeAllMainPanel
+        this.el.btnAttachCamera.on('click', e => {
             this.closeAllMainPanel();
             this.el.panelCamera.addClass('open');
             this.el.panelCamera.css({
                 height: 'calc(130% - 120px )'
             })
-         });
+        });
 
-         // fecha a camera e da um show nas mensagens
-         this.el.btnClosePanelCamera.on('click', e=>{
+        // fecha a camera e da um show nas mensagens
+        this.el.btnClosePanelCamera.on('click', e => {
             this.el.panelCamera.removeClass('open');
             this.el.panelMessagesContainer.show();
-         })
+        })
 
-         // tira a foto
-         this.el.btnTakePicture.on('click', e=>{
+        // tira a foto
+        this.el.btnTakePicture.on('click', e => {
             console.log('Foto')
-         })
+        })
 
-         // abre o elemento de escolher os documentos
-         this.el.btnAttachDocument.on('click', e =>{
+        // abre o elemento de escolher os documentos
+        this.el.btnAttachDocument.on('click', e => {
             this.closeAllMainPanel()
             this.el.panelDocumentPreview.addClass('open')
             this.el.panelDocumentPreview.css({
                 height: 'calc(130% - 120px )'
             })
-         });
+        });
 
-         // fecha o elemento de escolher os documentos
-         this.el.btnClosePanelDocumentPreview.on('click', e=>{
+        // fecha o elemento de escolher os documentos
+        this.el.btnClosePanelDocumentPreview.on('click', e => {
             this.el.panelDocumentPreview.removeClass('open');
             this.el.panelMessagesContainer.show();
 
-         })
+        })
 
-         // envia o documento
-         this.el.btnSendDocument.on('click', e=>{
+        // envia o documento
+        this.el.btnSendDocument.on('click', e => {
             console.log(`Evento`)
-         })
-          
-        // mostra os contatos
-         this.el.btnAttachContact.on('click', e =>{
-            this.el.modalContacts.show()
-         });
+        })
 
-         // retira o modal dos contatos
-         this.el.btnCloseModalContacts.on('click', e=>{
+        // mostra os contatos
+        this.el.btnAttachContact.on('click', e => {
+            this.el.modalContacts.show()
+        });
+
+        // retira o modal dos contatos
+        this.el.btnCloseModalContacts.on('click', e => {
             this.el.modalContacts.hide()
-         })
+        })
+
+        // inicia a gravação do audio
+        this.el.btnSendMicrophone.on('click', e => {
+            this.el.recordMicrophone.show();
+            this.el.btnSendMicrophone.hide();
+            this.startRecordMicrophoneTime();
+        })
+
+        // envia o audio
+        this.el.btnFinishMicrophone.on('click', e => {
+            this.closeRecordMicrophone();
+        })
+
+        // cancela o audio
+        this.el.btnCancelMicrophone.on('click', e => {
+            this.closeRecordMicrophone();
+        })
 
     };
 
+    // contato o tempo do audio
+    startRecordMicrophoneTime(){
+
+    let start = Date.now();
+
+    this._recordMicrophoneInterval = setInterval(()=>{
+        this.el.recordMicrophoneTimer.innerHTML = (Date.now() - start);
+    },1000)
+
+    }
+
+    closeRecordMicrophone() {
+        this.el.recordMicrophone.hide();
+        this.el.btnSendMicrophone.show();
+        clearInterval(this._recordMicrophoneInterval)
+    }
+
     // fecha alguns elementos para não fica repetitivo 
-    closeAllMainPanel(){
+    closeAllMainPanel() {
         this.el.panelMessagesContainer.hide();
         this.el.panelDocumentPreview.removeClass('open');
         this.el.panelCamera.removeClass('open');
     }
 
-        // Método para fechar o menu de anexos
+    // Método para fechar o menu de anexos
     closeMenuAttach(event) {
         document.removeEventListener('click', this.closeMenuAttach)
         this.el.menuAttach.removeClass('open')
